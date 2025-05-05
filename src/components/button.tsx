a2eabe12e805270f40e3e948eb582677afc8abf0
Jelle Kuipers <jellekuipers@Jelles-MacBook-Air.local>
2025-05-05 19:39:20 +0200

    1.0.2

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,4 +1,4 @@
-import { type GestureResponderEvent, Pressable, Text } from "react-native";
+import { type GestureResponderEvent, Pressable, Text } from 'react-native';
 
 interface ButtonProps {
   className?: string;
@@ -9,7 +9,7 @@ interface ButtonProps {
 export function Button({ children, onPress }: ButtonProps) {
   return (
     <Pressable
-      className="flex flex-row h-8 items-center gap-2 rounded border px-2 bg-black"
+      className="flex flex-row h-10 items-center gap-2 rounded border px-4 bg-black"
       onPress={onPress}
     >
       <Text className="text-base font-medium text-white antialiased">
